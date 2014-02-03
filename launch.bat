c8cac016df001932c1c0a43220f1dce6442a2a2b
Robert G <robzbakk@hotmail.co.uk>
2014-02-03 15:50:15 +0000

    no message

--- launch.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java -cp RSBot-5058.jar;./bin/ org.powerbot.Boot -Xss6m -Xmx2048m -XX:MaxPermSize=512m -XX:+CMSClassUnloadingEnabled -XX:+UseCodeCacheFlushing -XX:-UseSplitVerifier -XX:+UseConcMarkSweepGC -debug
-pause
