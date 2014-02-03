6edbf386f902505fb564762120408f59d24dc611
Robert G <robzbakk@hotmail.co.uk>
2014-02-03 17:47:06 +0000

    no message

--- scripts/util/Util.java
+++ scripts/util/Util.java
@@ -25,5 +25,5 @@ public class Util {
 		final int sec = (int) (time / 1000), h = sec / 3600, m = sec / 60 % 60, s = sec % 60;
 		return (h < 10 ? "0" + h : h) + ":" + (m < 10 ? "0" + m : m) + ":" + (s < 10 ? "0" + s : s);
     }
-
+	
 }
